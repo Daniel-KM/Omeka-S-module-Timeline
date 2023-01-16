b78b5018569f321582e01075ab099280f2c021d4
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-16 00:00:00 +0000

    Fixed form.

--- src/Service/Form/TimelineExhibitFieldsetFactory.php
+++ src/Service/Form/TimelineExhibitFieldsetFactory.php
@@ -1,4 +1,5 @@
 <?php declare(strict_types=1);
+
 namespace Timeline\Service\Form;
 
 use Interop\Container\ContainerInterface;
@@ -10,7 +11,7 @@ class TimelineExhibitFieldsetFactory implements FactoryInterface
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
         $urlHelper = $services->get('ViewHelperManager')->get('url');
-        $form = new TimelineExhibitFieldset(null, $options);
+        $form = new TimelineExhibitFieldset(null, $options ?? []);
         return $form
             ->setUrlHelper($urlHelper);
     }
